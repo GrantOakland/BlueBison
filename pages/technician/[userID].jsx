a447642604447a511c5b42ccaeaa150ef20fbe39
Grant Gryczan <grantgryczan@gmail.com>
2022-04-10 20:11:44 -0400

    Add contact info to technician profile

--- pages/technician/[userID].jsx
+++ pages/technician/[userID].jsx
@@ -1,21 +1,27 @@
 import ButtonLink from 'components/ButtonLink';
 import { dbQuery } from 'lib/db';
 import { sqlNumber } from 'lib/sql';
+import withErrorPage from 'lib/withErrorPage';
+import withStatusCode from 'lib/withStatusCode';
 
-const Component = ({ user }) => (
+const Component = withErrorPage(({ user }) => (
 	<>
 		<table>
 			<tr>
 				<th>First Name</th>
 				<th>Last Name</th>
-				<th>Email Address</th>
+				{user.USER_EMAIL && (
+					<th>Email Address</th>
+				)}
 				<th>Technician Level</th>
 				<th>User ID</th>
 			</tr>
 			<tr>
 				<td>{user.USER_FNAME}</td>
 				<td>{user.USER_LNAME}</td>
-				<td>{user.USER_EMAIL || 'N/A'}</td>
+				{user.USER_EMAIL && (
+					<td>{user.USER_EMAIL}</td>
+				)}
 				<td>{user.TECHNICIAN_LEVEL}</td>
 				<td>{user.USER_ID}</td>
 			</tr>
@@ -27,18 +33,18 @@ const Component = ({ user }) => (
 		<br />
 		<ButtonLink href="/">Go Back Home</ButtonLink>
 	</>
-);
+));
 
 export default Component;
 
-export const getServerSideProps = async ({ query }) => ({
+export const getServerSideProps = withStatusCode(async ({ req, query }) => ({
 	props: {
 		user: (await dbQuery(`
-			SELECT U.USER_ID, USER_FNAME, USER_LNAME, TECHNICIAN_LEVEL
+			SELECT U.USER_ID, USER_FNAME, USER_LNAME, TECHNICIAN_LEVEL${req.user.USER_ID === +query.userID || req.user.USER_IS_TECHNICIAN ? ', USER_EMAIL' : ''}
 			FROM USER AS U
 			INNER JOIN TECHNICIAN AS T
 			ON U.USER_ID = T.USER_ID
 			WHERE U.USER_ID = ${sqlNumber(query.userID)}
 		`))[0]
 	}
-});
+}));
