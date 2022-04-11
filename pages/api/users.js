29989c06bb214ebd8cdeb7978095eb745cb1325c
Grant Gryczan <grantgryczan@gmail.com>
2022-04-11 00:42:29 -0400

    Signup

---
+++ pages/api/users.js
@@ -0,0 +1,47 @@
+import { dbQuery } from 'lib/db';
+import Cookies from 'cookies';
+import { sqlString } from 'lib/sql';
+import newID from 'lib/newID';
+
+export default async function handler(req, res) {
+	if (req.method === 'POST') {
+		const userID = await newID('USER');
+
+		const email = req.body.email.toLowerCase();
+
+		if ((await dbQuery(`
+			SELECT *
+			FROM USER
+			WHERE USER_EMAIL = ${sqlString(email)}
+		`)).length) {
+			res.status(422).send('A user with that email already exists.');
+			return;
+		}
+
+		await dbQuery(`
+			INSERT INTO USER VALUES (
+				${userID},
+				0,
+				${sqlString(email)},
+				${sqlString(req.body.password)},
+				${sqlString(req.body.fname)},
+				${sqlString(req.body.lname)}
+			)
+		`);
+		await dbQuery(`
+			INSERT INTO CUSTOMER VALUES (
+				${userID},
+				${sqlString(req.body.phone)},
+				${sqlString(req.body.timeZone)}
+			)
+		`);
+
+		const cookies = new Cookies(req, res);
+		cookies.set('user', userID, {
+			sameSite: 'strict',
+			maxAge: 1000 * 60 * 60 * 24 * 7
+		});
+
+		res.status(201).end();
+	}
+}
--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,35 +1,58 @@
 import ButtonLink from 'components/ButtonLink';
+import { Field, Form, Formik } from 'formik';
+import api from 'lib/api';
+import useFunction from 'lib/useFunction';
 
 const Component = () => (
 	<>
 		{/* Will need to add the action to submit this user data to the user table in the DB */}
 		<h2>Sign Up</h2>
-		<p>Please fill in this form to create an account. All fields are required except time zone.</p>
+		<p>Please fill in this form to create an account.</p>
 		<hr />
 
-		<label htmlFor="fname"><b>First Name:</b></label>
-		<input type="text" placeholder="Enter First Name" name="fname" required />
-		<br />
-		<label htmlFor="lname"><b>Last Name:</b></label>
-		<input type="text" placeholder="Enter Last Name" name="lname" required />
-		<br />
-		<label htmlFor="email"><b>Email:</b></label>
-		<input type="text" placeholder="Enter Email" name="email" required />
-		<br />
-		<label htmlFor="phoneNumber"><b>Phone Number:</b></label>
-		<input type="text" placeholder="Enter your phone number" name="phoneNumber" required />
-		<br />
-		<label htmlFor="psw"><b>Password:</b></label>
-		<input type="password" placeholder="Enter Password" name="psw" required />
-		<br />
-		<label htmlFor="timeZone"><b>Time Zone:</b></label>
-		<input type="text" placeholder="Enter in your Time Zone Abbreviation" name="timeZone" />
-		<br />
-		<br />
+		<Formik
+			initialValues={{
+				email: '',
+				password: '',
+				fname: '',
+				lname: '',
+				phone: '',
+				timeZone: ''
+			}}
+			onSubmit={
+				useFunction(async values => {
+					await api.post('/users', values);
 
-		<div>
-			<button type="submit" className="signupbtn">Sign Up</button>
-		</div>
+					location.href = '/dashboard';
+				})
+			}
+		>
+			<Form>
+				<label htmlFor="email"><b>Email*:</b></label>
+				<Field type="text" placeholder="Email" name="email" id="email" required autoFocus />
+				<br />
+				<label htmlFor="password"><b>Password*:</b></label>
+				<Field type="password" placeholder="Password" name="password" id="password" required />
+				<br />
+				<label htmlFor="fname"><b>First Name*:</b></label>
+				<Field type="text" placeholder="First Name" name="fname" id="fname" required />
+				<br />
+				<label htmlFor="lname"><b>Last Name*:</b></label>
+				<Field type="text" placeholder="Last Name" name="lname" id="lname" required />
+				<br />
+				<label htmlFor="phone"><b>Phone:</b></label>
+				<Field type="phone" placeholder="Phone Number" name="phone" id="phone" />
+				<br />
+				<label htmlFor="time-zone"><b>Time Zone:</b></label>
+				<Field type="text" placeholder="Time Zone Abbreviation" name="timeZone" id="time-zone" />
+				<br />
+				<br />
+
+				<div>
+					<button type="submit">Sign Up</button>
+				</div>
+			</Form>
+		</Formik>
 		<br />
 
 		<ButtonLink href="/login">Go back to login page.</ButtonLink>
